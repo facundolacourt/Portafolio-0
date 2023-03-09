86541fda7c1ea4a091972ef8a79a869b7cf8441c
facundolacourt <86851718+facundolacourt@users.noreply.github.com>
2023-03-09 13:15:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Super Junior", "Buen Amante"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+})
